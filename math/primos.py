926481b78551975ff2c7f958c5d628cc20e55df9
Manuel Maldonado <manuel.maldonado@lextrend.com>
2018-12-30 18:52:40 +0100

    Prime number generator

--- math/primos.py
+++ math/primos.py
@@ -1,24 +1,19 @@
-
+import sys
 def get_primos() :
-    i = 4
+    i = 1
+    j = 1
     while True :
         if is_primo(i) : 
-            print(i)
-        i = i + 1
+            print("Primo numero ", j," : ",i)
+            j += 1
+        i += 1
+        
     
     return True
 
 def is_primo(n) : 
-    primo = True
-    if n%2 == 0 :
-        primo = False
-    if n%5 == 0 :
-        primo = False
-    if split_digits_sum(n)%3 == 0 : 
-        primo = False
-    if is_divisible(n) : 
-        primo = False
-    return primo
+    if n%2 == 0 or is_divisible(n) : return False
+    return True
 
 def split_digits_sum(n) :
 	sum = 0
@@ -28,7 +23,18 @@ def split_digits_sum(n) :
 	return sum
 
 def is_divisible(n) : 
-    return True
+    for i in range(3,n//2,2) :
+        if n%i == 0 :
+            return True
+    return False
 
-print('Hola de nuevo caraculo')
-get_primos()
+
+#print('Aquí van unos cuantos números primos...')
+#get_primos()
+if len(sys.argv) < 2 :
+    print("Introduce el número que quieres comprobar")
+else : 
+    if is_primo(int(sys.argv[1])) :
+        print("Primo!!!")
+    else :
+        print("No primo")
